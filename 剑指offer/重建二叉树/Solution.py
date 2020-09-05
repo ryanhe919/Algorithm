05fe3a3bd561b8d477bb13e80959f26494ec3ebd
Yufan <ryanhe0919@gmail.com>
2020-09-05 21:09:16 +0800

    finish buildTree

---
+++ 剑指offer/重建二叉树/Solution.py
@@ -0,0 +1,8 @@
+def buildTree(preorder, inorder):
+    if len(preorder) == 0 or len(inorder) == 0:
+        return None
+    root = TreeNode(preorder[0])
+    left_length = inorder.index(preorder[0]) + 1
+    root.left = buildTree(preorder[1:left_length], inorder[0:left_length - 1])
+    root.right = buildTree(preorder[left_length:], inorder[left_length:])
+    return root
